cce6cd3d8bfd648e9cca86b92b88a85658b24c11
yonggang <yonggang.zhang@17zuoye.com>
2018-06-02 16:00:39 +0800

    master

---
+++ src/msggen2openapi.ts
@@ -0,0 +1,217 @@
+import OpenAPI, {Server, Request, Parameter, Response, Schema, SchemaProperty, SchemaObject} from "./openapi";
+import * as fs from "fs";
+import * as xml2js from 'xml2js';
+import * as path from "path";
+
+function msggen2Openapi(msggenPath: string, openapiPath: string, host?): boolean {
+    let msggen = null;
+
+    let openAPI = new OpenAPI();
+
+    if (host instanceof Array) {
+        for (let item of host) {
+            openAPI.addServer(new Server("https://"+item+".test.17zuoye.net", ""));
+            openAPI.addServer(new Server("https://"+item+".staging.17zuoye.net", ""));
+            openAPI.addServer(new Server("https://"+item+".17zuoye.com", ""));
+        }
+    }
+    else {
+        openAPI.addServer(new Server("https://"+host+".test.17zuoye.net", ""));
+        openAPI.addServer(new Server("https://"+host+".staging.17zuoye.net", ""));
+        openAPI.addServer(new Server("https://"+host+".17zuoye.com", ""));
+    }
+
+    if (fs.existsSync(msggenPath)) {
+        if (fs.statSync(msggenPath).isDirectory) {
+            for (let filePath of fs.readdirSync(msggenPath)) {
+                if (!readMsgFromFile(msggenPath+"/"+filePath, openAPI)) {
+                    return false;
+                }
+            }
+        }
+        else {
+            return readMsgFromFile(msggenPath, openAPI);
+        }
+    }
+
+    console.log(openAPI.dump());
+
+    return true;
+}
+
+function readMsgFromFile(msggenPath: string, openAPI: OpenAPI): boolean {
+    let msggen = null;
+
+    if (msggenPath.toLowerCase().endsWith(".json")) {
+        msggen = JSON.parse(fs.readFileSync(msggenPath).toString());
+    }
+    else if (msggenPath.toLowerCase().endsWith(".xml")) {
+        (new xml2js.Parser({explicitArray : false})).parseString(fs.readFileSync(msggenPath).toString(), (err, result) => {
+            msggen = result;
+        });
+    }
+    else {
+        console.error("file " + msggen + " is not supported");
+        return false;
+    }
+
+    return convert2OpenAPI(msggen, openAPI, path.basename(msggenPath, path.extname(msggenPath)));
+}
+
+function addComponent(openAPI: OpenAPI, item: any, componentName: string) {
+    let schemaResponse = new SchemaObject();
+
+    if (item.field) {
+        if (item.field instanceof Array) {
+
+            for (let field of item.field) {
+
+                let schema = new SchemaProperty(field.$.type);
+                schema.description = field.$.comment;
+                
+                schemaResponse.addProperty(field.$.name, schema);
+
+                //let schema = new Schema(item.$.name+"Response");
+                
+                // let response = new Response();
+                // response.content.addSchema(schema);
+                // request.addResponse(response);
+            }
+        }
+        else {
+            let schema = new SchemaProperty(item.field.$.type);
+            schema.description = item.field.$.comment;
+            
+            schemaResponse.addProperty(item.field.$.name, schema);
+
+            //let schema = new Schema(item.$.name+"Response");
+            //let schema = new SchemaProperty(item.$.name+"Response");
+
+            // let response = new Response();
+            // response.content.addSchema(schema);
+            // request.addResponse(response);
+        }
+    }
+
+    openAPI.components.addSchema(componentName, schemaResponse);
+}
+
+function createRequest(openAPI: OpenAPI, item: any, defaultMethod: string): Request {
+    let request = new Request(item.$.name, item.$.method?item.$.method:defaultMethod);
+    request.summary = item.$.comment;
+
+    if (item.field) {
+        if (item.field instanceof Array) {
+            for (let field of item.field) {
+                let parameter = new Parameter();
+                parameter.name = field.$.name;
+                parameter.description = field.$.comment;
+                parameter.schemaType = field.$.type;
+                request.addParameter(parameter);
+            }
+        }
+        else {
+            let parameter = new Parameter();
+            parameter.name = item.field.$.name;
+            parameter.description = item.field.$.comment;
+            parameter.schemaType = item.field.$.type;
+            request.addParameter(parameter);
+        }
+    }
+
+    return request;
+}
+
+function addLoginAPI(openAPI: OpenAPI) {
+    let request = new Request("登陆", "GET");
+    request.addParameter(new Parameter('user_code', 'string'));
+    request.addParameter(new Parameter('passwd', 'string'));
+    request['security'] = [];
+    let response = new Response();
+    response.content.addSchema(new Schema("DevLogin"+"Response"))
+    request.addResponse(response)
+
+    let schemaResponse = new SchemaObject();
+    let dataSchema = new SchemaProperty('object');
+    dataSchema['additionalProperties'] = true;
+    schemaResponse.addProperty("success", new SchemaProperty("boolean"));
+    schemaResponse.addProperty("data", dataSchema);
+    openAPI.components.addSchema("DevLogin"+"Response", schemaResponse);
+
+    openAPI.addApi('/dev/login', request);
+}
+
+function convert2OpenAPI(msggen: any, openAPI: OpenAPI, tag?: string): boolean {
+    let defaultProtocol = 'http';
+    let defaultMethod = 'GET';
+    let defaultContentType = null;
+
+    if (msggen.config && msggen.config.type && msggen.config.type.extra && msggen.config.type.extra.item) {
+        if (msggen.config.type.extra.item instanceof Array) {
+            for (let item of msggen.config.type.extra.item) {
+                addComponent(openAPI, item, item.$.name)
+            }
+        }
+        else {
+            addComponent(openAPI, msggen.config.type.extra.item, msggen.config.type.extra.item.$.name)
+        }
+    }
+
+    if (msggen.config && msggen.config.message && msggen.config.message.default && msggen.config.message.default.$) {
+        if (msggen.config.message.default.$.method) {
+            defaultMethod = msggen.config.message.default.$.method;
+        }
+
+        if (msggen.config.message.default.$.protocol) {
+            defaultProtocol = msggen.config.message.default.$.protocol;
+        }
+
+        if (msggen.config.message.default.$.contentType) {
+            defaultContentType = msggen.config.message.default.$.contentType;
+        }
+    }
+
+    addLoginAPI(openAPI);
+
+    if (msggen.config && msggen.config.message && msggen.config.message.extra && msggen.config.message.extra.item) {
+        for (let item of msggen.config.message.extra.item) {
+            let request = createRequest(openAPI, item, defaultMethod);
+            if (tag) {
+                request.addTag(tag);
+            }
+
+            if (msggen.config.response && msggen.config.response.extra && msggen.config.response.extra.item) {
+                if (msggen.config.response.extra.item instanceof Array) {
+                    for (let item of msggen.config.response.extra.item) {
+                        if (item.$.name == request.name) {
+                            let response = new Response();
+                            let schema = new Schema(item.$.name+"Response");
+                            response.content.addSchema(schema);
+                            response.description = item.$.comment;
+                            request.addResponse(response);
+
+                            addComponent(openAPI, item, item.$.name+"Response")
+
+                            break;
+                        }
+                    }
+                }
+            }
+
+            if (!request.responses) {
+                let schema = new Schema(item.$.name+"Response");
+                let response = new Response();
+                response.content.addSchema(schema);
+                request.addResponse(response);
+
+                addComponent(openAPI, {}, item.$.name+"Response")
+            }
+
+            openAPI.addApi(item.$.url, request);
+        }
+    }
+
+    return true;
+}
+
+export default msggen2Openapi;
---
+++ src/openapi.ts
@@ -0,0 +1,291 @@
+import * as jsYaml from "js-yaml"
+import { ENAMETOOLONG } from "constants";
+
+export class Server {
+    public url: string;
+    public description: string;
+
+    public constructor(url: string, description: string="") {
+        this.url = url;
+        this.description = description;
+    }
+}
+
+class Paths {
+    private _path;
+    public get path(): string {
+        return this._path;
+    }
+
+    private _requests;
+    public get requests(): Request[] {
+        return this._requests;
+    }
+
+    public addRequest(path: string, request: Request):void {
+        if (path.length) {
+            if (!this[path]) {
+                this[path] = {
+                };
+            }
+
+            this[path][request.method.toLowerCase()] = request;
+        }
+    }
+}
+
+export class Schema {
+    constructor(schemaType: string) {
+        this.setType(schemaType);
+    }
+
+    private setType(schemaType: string) {
+        if (schemaType.endsWith('[]')) {
+            this['type'] = 'array';
+
+            this["items"] = new Schema(schemaType.slice(0, schemaType.length-2))
+        }
+        else {
+            let matched = schemaType.match(/\{(\w+):(\w+)\}/)
+            if (matched) {
+                this['type'] = 'object';
+
+                this['description'] = schemaType;
+                this['additionalProperties'] = new Schema(matched[2])
+            }
+            else {
+                if (this.isBasicType(schemaType)) {
+                    this['type'] = this.standardBasicType(schemaType);
+                }
+                else {
+                    this['$ref'] = '#/components/schemas/'+schemaType;
+                }
+            }
+        }
+    }
+
+    private isBasicType(type: string) {
+        return ["string", "number", "int", "integer", "long", "float", "boolean", "array", "object", "any"].indexOf(type) >= 0;
+    }
+
+    private standardBasicType(type: string) {
+        switch(type) {
+            case "int":
+            case "long":
+                return "integer";
+            case "float":
+                return "number";
+            case "any":
+                return "object";
+            default:
+                return type;
+        }
+    }
+}
+
+export class Parameter {
+    public in: string;
+    public name: string;
+    private schema: Schema;
+    public required: boolean;
+    public description: string;
+
+    constructor(name?: string, schemaType?: string) {
+        this.in = 'query';
+        if (name) {
+            this.name = name;
+        }
+        if (schemaType) {
+            this.schemaType = schemaType;
+        }
+    }
+
+    public set schemaType(schemaType: string) {
+        this.schema = new Schema(schemaType);
+    }
+}
+
+export class Request {
+    public method:string;
+    
+    public constructor(name: string, method: string) {
+        this.name = name;
+        this.method = method;
+        this.tags = [];
+
+        for (let propertyKey of ['name', 'method', '_tags']) {
+            let descriptor = Object.getOwnPropertyDescriptor(this, propertyKey) || {};
+            descriptor.enumerable = false;
+            Object.defineProperty(this, propertyKey, descriptor);
+        }
+    }
+
+    private tags: string[];
+    public addTag(tag: string) {
+        if (this.tags.indexOf(tag) == -1) {
+            this.tags.push(tag);
+        }
+    }
+
+    public summary: string;
+    public description:string;
+
+    public name:string;
+
+    public get $ref() {
+        return this.name?this.name+'Request':null;
+    }
+
+    public parameters: Parameter[];
+    public addParameter(parameter: Parameter) {
+        if (!this.parameters) {
+            this.parameters = [];
+        }
+
+        this.parameters.push(parameter);
+    }
+
+    public responses;
+    
+    public addResponse(response: Response, statusCode: string="200") {
+        if (!this.responses) {
+            this.responses = {};
+        }
+
+        this.responses[statusCode] = response;
+    }
+}
+
+export class Response {
+    constructor() {
+         this.content = new ResponseContent();
+         this.description = "";
+    }
+
+    public description: string;
+    public content: ResponseContent
+}
+
+export class ResponseContent {
+    public addSchema(schame: Schema, mimeType: string='application/json') {
+        this[mimeType] = {
+            schema: schame
+        };
+    }
+}
+
+export class SchemaProperty extends Schema {
+    public description: string;
+
+    constructor(schemaType: string) {
+        super(schemaType);
+    }
+}
+
+export class SchemaObject {
+    public type: string;
+    public properties: object;
+
+    constructor() {
+        this.type = "object";
+    }
+
+    public addProperty(name: string, schemaProperty: SchemaProperty) {
+        if (!this.properties) {
+            this.properties = {};
+        }
+
+        this.properties[name] = schemaProperty;
+    }
+}
+
+class Components {
+    public schemas;
+
+    constructor() {
+        this['securitySchemes'] = {
+            "SessionKeyAuth": {
+                type: "apiKey",
+                in: "query",
+                name: "session_key"
+            },
+            "cookieAuth": {
+                type: "apiKey",
+                in: "cookie",
+                name: "va_sess"
+            }
+        };
+    }
+
+    public addSchema(name: string, schemaObject: SchemaObject) {
+        if (!this.schemas) {
+            this.schemas = {};
+        }
+
+        this.schemas[name] = schemaObject;
+    }
+}
+
+class OpenAPI {
+    public title;
+    public description;
+    public version;
+    public components: Components;
+
+    public constructor() {
+        this.components = new Components();
+        this._servers = [];
+    }
+
+    public dump(): string
+    {
+        return jsYaml.dump({
+            openapi: '3.0.0',
+            info: {
+                title: '',
+                description: '',
+                version: ''
+            },
+            components: this.components,
+            security: [
+                {SessionKeyAuth: []},
+                {cookieAuth: []}
+            ],
+            servers: this.servers,
+            paths: this.paths
+        })
+    }
+
+    private _servers: Server[];
+    public get servers(): Server[] {
+        return this._servers;
+    }
+
+    public addServer(server:Server):void {
+        this._servers.push(server);
+    }
+
+    public addComponent(name: string, schemaObject: SchemaObject) {
+        if (!this.components) {
+            this.components = new Components();
+        }
+
+        this.components.addSchema(name, schemaObject);
+    }
+
+    private _paths: Paths;
+    public get paths(): Paths {
+        if (!this._paths) {
+            this._paths = new Paths();
+        }
+
+        return this._paths;
+    }
+
+    public addApi(path: string, request: Request) {
+        this.paths.addRequest(path, request);
+    }
+
+}
+
+export default OpenAPI;
