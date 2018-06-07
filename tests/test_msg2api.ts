d3db1789ac037dec26d1385a882c7f740a62dfeb
yonggang <yonggang.zhang@17zuoye.com>
2018-06-07 11:25:17 +0800

    compile option

--- tests/test_msg2api.ts
+++
@@ -1,4 +0,0 @@
-import msggen2Openapi from "../src/msggen2openapi";
-import * as fs from 'fs';
-
-msggen2Openapi("/Users/yonggang/17zuoye/src/vas/msggen/Nova/messages", './tests/openapi.yaml', ['novaeng', 'novamath', 'novachn']);
